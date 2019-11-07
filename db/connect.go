fd8e708ab975987c337e969c11d447224bab96cc
jieeiro sst <luumanhquan.91@gmail.com>
2019-11-07 11:32:39 +0700

    database model save info

---
+++ db/connect.go
@@ -0,0 +1,83 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	NameDB    = "mysql"
+	MysqlUser = "root"
+	MysqlPass = " "
+	MysqlDB   = "4chat"
+	MyPort    = "3306"
+	MyUrl     = "localhost"
+)
+
+var db *sql.DB
+
+func HandleDataBase() {
+	db, err := sql.Open(NameDB, MysqlUser+":"+MysqlPass+"@("+MyUrl+":"+MyPort+")/"+MysqlDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("connected to mysql database")
+}
+
+func SelectUser(id string) {
+	result, err := db.Query("SELECT UserName,Password from Account where id=?", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer result.Close()
+
+	UserNames := make([]string, 0)
+	Passwords := make([]string, 0)
+	for result.Next() {
+		var UserName string
+		var Password string
+		err := result.Scan(&UserName, &Password)
+		if err != nil {
+			log.Fatal(err)
+		}
+		UserNames = append(UserNames, UserName)
+		Passwords = append(Passwords, Password)
+	}
+
+	if err := result.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type project struct {
+	UserName string
+	Password string
+}
+
+var projects []project
+
+func InsertUser(UserName string, Password string) {
+	stmt, err := db.Prepare("insert into Account(id,Username,Passowrd) values(?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	//upppdate data
+	var p project
+	p.UserName = UserName
+	p.Password = Password
+
+	for id, user := range projects {
+		if err, _ := stmt.Exec(id+1,user.UserName, user.Password); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
